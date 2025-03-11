0ea15cf65e4bf9ba87065a265bba18e85bdf0f3c
Kian_School <24-0203c@CCL1-10>
2025-03-11 12:41:26 +0800

    Added 1st Update

--- DSaA_5/Program.cs
+++ DSaA_5/Program.cs
@@ -10,6 +10,32 @@ namespace DSaA_5
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int[] mainArray = new int[100];
+            int[] genCounter = new int[100];
+            
+            int avgCount = 0;
+
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                int genCount = 0;
+                int tempNum;
+                if (i == 0)
+                    mainArray[i] = rnd.Next(1,101);
+                else
+                {
+                    tempNum = rnd.Next(1, 101);
+                    for (int j = 0; j < i; j++)
+                    {
+                        
+                        genCount++;
+                        if (tempNum == mainArray[j])
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
